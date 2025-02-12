f18131d9e5689dba722ded22b62b9b32f271a0df
Parshva <codewithpasshu@gmail.com>
2025-02-12 10:41:11 +0530

    footer

--- src/Common/Footer.js
+++ src/Common/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
               <div className="row footer-top-28">
                 <div className="col-lg-6 footer-list-28">
                   <h2 className="footer-title-28">
-                    <a href="index.html"> Estate Property </a>
+                    <a to="index.html"> Estate Property </a>
                   </h2>
                   <p className="mt-4">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit,
@@ -22,22 +22,22 @@ const Footer = () => {
                   <div className="main-social-footer-28 mt-3">
                     <ul className="social-icons">
                       <li className="facebook">
-                        <a href="#link" title="Facebook">
+                        <a to="#link" title="Facebook">
                           <span className="fa fa-facebook" aria-hidden="true" />
                         </a>
                       </li>
                       <li className="twitter">
-                        <a href="#link" title="Twitter">
+                        <a to="#link" title="Twitter">
                           <span className="fa fa-twitter" aria-hidden="true" />
                         </a>
                       </li>
                       <li className="dribbble">
-                        <a href="#link" title="Dribbble">
+                        <a to="#link" title="Dribbble">
                           <span className="fa fa-dribbble" aria-hidden="true" />
                         </a>
                       </li>
                       <li className="google">
-                        <a href="#link" title="Google">
+                        <a to="#link" title="Google">
                           <span className="fa fa-google" aria-hidden="true" />
                         </a>
                       </li>
@@ -70,19 +70,19 @@ const Footer = () => {
                       <h6 className="footer-title-28">Recent Posts</h6>
                       <div className="fot-top row mt-4">
                         <div className="col-4">
-                          <a href="blog-single.html">
+                          <Link to="/blogsingle">
                             <img
                               src="assets/images/g1.jpg"
                               className="img-responsive"
                               alt
                             />
-                          </a>
+                          </Link>
                         </div>
                         <div className="col-8 pl-2">
                           <h5>
-                            <a href="blog-single.html">
+                            <Link to="/blogsingle">
                               We Are Leading International Consultiing Agency
-                            </a>
+                            </Link>
                           </h5>
                           <p>
                             <span className="fa fa-clock-o" /> December 17, 2019
@@ -91,20 +91,20 @@ const Footer = () => {
                       </div>
                       <div className="fot-top row mt-4">
                         <div className="col-4">
-                          <a href="blog-single.html">
+                          <Link to="/blogsingle">
                             <img
                               src="assets/images/g2.jpg"
                               className="img-responsive"
                               alt
                             />
-                          </a>
+                          </Link>
                         </div>
                         <div className="col-8 pl-2">
                           <h5>
                             {" "}
-                            <a href="blog-single.html">
+                            <Link to="/blogsingle">
                               We Are Leading International Consultiing Agency
-                            </a>
+                            </Link>
                           </h5>
                           <p>
                             {" "}
@@ -120,15 +120,7 @@ const Footer = () => {
             <div className="midd-footer-28 text-center py-lg-4 py-3 mt-5">
               <div className="container">
                 <p className="copy-footer-28 text-center">
-                  {" "}
                   © 2020 Estate Property. All Rights Reserved.Design by
-                  <a
-                    href="https://w3layouts.com/"
-                    className="link-cory-right"
-                    target="_blank"
-                  >
-                    W3layouts
-                  </a>
                 </p>
               </div>
             </div>
