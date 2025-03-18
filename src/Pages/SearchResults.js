81fc6298ef8df72b05d912780869c49726d9511d
Parshva <codewithpasshu@gmail.com>
2025-03-18 20:01:08 +0530

    Changes

--- src/Pages/SearchResults.js
+++ src/Pages/SearchResults.js
@@ -4,6 +4,57 @@ import Header from "../Common/Header";
 import Banner from "../Common/Banner";
 import { Link } from "react-router-dom";
 
+const properties = [
+  {
+    image: "assets/images/g12.jpg",
+    title: "Luxury Apartment in Bandra",
+    location: "Mumbai, Maharashtra, India",
+    beds: 4,
+    baths: 3,
+    size: "1800 sq ft",
+  },
+  {
+    image: "assets/images/g11.jpg",
+    title: "Spacious Villa in Whitefield",
+    location: "Bangalore, Karnataka, India",
+    beds: 3,
+    baths: 2,
+    size: "2200 sq ft",
+  },
+  {
+    image: "assets/images/g10.jpg",
+    title: "Modern Flat in Connaught Place",
+    location: "Delhi, India",
+    beds: 2,
+    baths: 2,
+    size: "1400 sq ft",
+  },
+  {
+    image: "assets/images/g9.jpg",
+    title: "Beachside Villa in ECR",
+    location: "Chennai, Tamil Nadu, India",
+    beds: 5,
+    baths: 4,
+    size: "2500 sq ft",
+  },
+  {
+    image: "assets/images/g8.jpg",
+    title: "Heritage Home in Salt Lake",
+    location: "Kolkata, West Bengal, India",
+    beds: 3,
+    baths: 2,
+    size: "1900 sq ft",
+  },
+  {
+    image: "assets/images/g7.jpg",
+    title: "Premium Bungalow in Jubilee Hills",
+    location: "Hyderabad, Telangana, India",
+    beds: 6,
+    baths: 5,
+    size: "3000 sq ft",
+  },
+];
+
 const SearchResults = () => {
   return (
     <div>
@@ -15,187 +66,47 @@ const SearchResults = () => {
   );
 };
 
-function Main() {
+const Main = () => {
   return (
-    <>
-      <section className="grids-2 m-auto" style={{ width: "75%" }}>
-        <div id="grids4-block" className="py-5">
-          <div className="container py-md-3">
-            <div className="row mt-5 pt-3">
-              <div className="grids4-info col-lg-4 col-md-6">
+    <section className="grids-2 m-auto" style={{ width: "75%" }}>
+      <div id="grids4-block" className="py-5">
+        <div className="container py-md-3">
+          <div className="row mt-5 pt-3">
+            {properties.map((property, index) => (
+              <div className="grids4-info col-lg-4 col-md-6 mt-4" key={index}>
                 <Link id="link" to="/properties-single">
                   <img
-                    src="assets/images/g12.jpg"
-                    className="img-fluid"
-                    alt=""
+                    src={property.image}
+                    className="img-fluid rounded shadow-sm"
+                    alt={property.title}
                   />
                 </Link>
-                <div className="info-bg">
+                <div className="info-bg p-3">
                   <h5>
                     <Link id="link" to="/properties-single">
-                      Luxury Apartment in Bandra
+                      {property.title}
                     </Link>
                   </h5>
-                  <p>Mumbai, Maharashtra, India</p>
-                  <ul>
+                  <p>{property.location}</p>
+                  <ul className="list-unstyled d-flex gap-2 flex-wrap">
                     <li>
-                      <span className="fa fa-bed" /> 4 Beds
+                      <span className="fa fa-bed" /> {property.beds} Beds
                     </li>
                     <li>
-                      <span className="fa fa-bath" /> 3 Baths
+                      <span className="fa fa-bath" /> {property.baths} Baths
                     </li>
                     <li>
-                      <span className="fa fa-share-square-o" /> 1800 sq ft
+                      <span className="fa fa-share-square-o" /> {property.size}
                     </li>
                   </ul>
                 </div>
               </div>
-              <div className="grids4-info col-lg-4 col-md-6 mt-md-0 mt-4">
-                <Link id="link" to="/properties-single">
-                  <img
-                    src="assets/images/g11.jpg"
-                    className="img-fluid"
-                    alt=""
-                  />
-                </Link>
-                <div className="info-bg">
-                  <h5>
-                    <Link id="link" to="/properties-single">
-                      Spacious Villa in Whitefield
-                    </Link>
-                  </h5>
-                  <p>Bangalore, Karnataka, India</p>
-                  <ul>
-                    <li>
-                      <span className="fa fa-bed" /> 3 Beds
-                    </li>
-                    <li>
-                      <span className="fa fa-bath" /> 2 Baths
-                    </li>
-                    <li>
-                      <span className="fa fa-share-square-o" /> 2200 sq ft
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="grids4-info col-lg-4 col-md-6 mt-lg-0 mt-4">
-                <Link id="link" to="/properties-single">
-                  <img
-                    src="assets/images/g10.jpg"
-                    className="img-fluid"
-                    alt=""
-                  />
-                </Link>
-                <div className="info-bg">
-                  <h5>
-                    <Link id="link" to="/properties-single">
-                      Modern Flat in Connaught Place
-                    </Link>
-                  </h5>
-                  <p>Delhi, India</p>
-                  <ul>
-                    <li>
-                      <span className="fa fa-bed" /> 2 Beds
-                    </li>
-                    <li>
-                      <span className="fa fa-bath" /> 2 Baths
-                    </li>
-                    <li>
-                      <span className="fa fa-share-square-o" /> 1400 sq ft
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="grids4-info col-lg-4 col-md-6 mt-4">
-                <Link id="link" to="/properties-single">
-                  <img
-                    src="assets/images/g9.jpg"
-                    className="img-fluid"
-                    alt=""
-                  />
-                </Link>
-                <div className="info-bg">
-                  <h5>
-                    <Link id="link" to="/properties-single">
-                      Beachside Villa in ECR
-                    </Link>
-                  </h5>
-                  <p>Chennai, Tamil Nadu, India</p>
-                  <ul>
-                    <li>
-                      <span className="fa fa-bed" /> 5 Beds
-                    </li>
-                    <li>
-                      <span className="fa fa-bath" /> 4 Baths
-                    </li>
-                    <li>
-                      <span className="fa fa-share-square-o" /> 2500 sq ft
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="grids4-info col-lg-4 col-md-6 mt-4">
-                <Link id="link" to="/properties-single">
-                  <img
-                    src="assets/images/g8.jpg"
-                    className="img-fluid"
-                    alt=""
-                  />
-                </Link>
-                <div className="info-bg">
-                  <h5>
-                    <Link id="link" to="/properties-single">
-                      Heritage Home in Salt Lake
-                    </Link>
-                  </h5>
-                  <p>Kolkata, West Bengal, India</p>
-                  <ul>
-                    <li>
-                      <span className="fa fa-bed" /> 3 Beds
-                    </li>
-                    <li>
-                      <span className="fa fa-bath" /> 2 Baths
-                    </li>
-                    <li>
-                      <span className="fa fa-share-square-o" /> 1900 sq ft
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="grids4-info col-lg-4 col-md-6 mt-4">
-                <Link id="link" to="/properties-single">
-                  <img
-                    src="assets/images/g7.jpg"
-                    className="img-fluid"
-                    alt=""
-                  />
-                </Link>
-                <div className="info-bg">
-                  <h5>
-                    <Link id="link" to="/properties-single">
-                      Premium Bungalow in Jubilee Hills
-                    </Link>
-                  </h5>
-                  <p>Hyderabad, Telangana, India</p>
-                  <ul>
-                    <li>
-                      <span className="fa fa-bed" /> 6 Beds
-                    </li>
-                    <li>
-                      <span className="fa fa-bath" /> 5 Baths
-                    </li>
-                    <li>
-                      <span className="fa fa-share-square-o" /> 3000 sq ft
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
-}
+};
 
 export default SearchResults;
